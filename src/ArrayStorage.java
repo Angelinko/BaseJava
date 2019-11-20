8e59a81bf10544dfd35b9a7ab204c2970a13492a
Angelina S <Angelinko.lol@gmail.com>
2019-11-20 17:07:38 +0300

    HW №1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,70 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-//
+
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+       return resume;
+
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < storage.length - 1; j++) {
+                    storage[j] = storage[j + 1];
+                    if (storage[j] == null) {
+                        storage[j - 1] = null;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int newLength = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                newLength ++;
+            }
+        }
+        return Arrays.copyOf(storage, newLength);
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                size = i + 1;
+            }
+        }
+        return size;
     }
 }
